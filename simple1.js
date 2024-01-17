464c948a300e31efcf0a9af2c3b94d67451a7354
yungan9 <geyun105@naver.com>
2024-01-17 09:16:27 +0900

    render

--- simple1.js
+++ simple1.js
@@ -7,5 +7,9 @@ function LikeButton() {
     text
   );
 }
-const domContainer = document.getElementById("root");
-ReactDOM.render(React.createElement(LikeButton), domContainer);
+const domContainer1 = document.getElementById("root1");
+ReactDOM.render(React.createElement(LikeButton), domContainer1);
+const domContainer2 = document.getElementById("root2");
+ReactDOM.render(React.createElement(LikeButton), domContainer2);
+const domContainer3 = document.getElementById("root3");
+ReactDOM.render(React.createElement(LikeButton), domContainer3);
