13c472955f1849326431e8b47eaad83b54a2a72f
yungan9 <geyun105@naver.com>
2024-01-20 23:53:11 +0900

    jsx 변환

--- simple1.js
+++ simple1.js
@@ -1,15 +1,22 @@
 function LikeButton() {
   const [liked, setLiked] = React.useState(false);
   const text = liked ? "좋아요 취소" : "좋아요";
-  return React.createElement(
-    "button",
-    { onClick: () => setLiked(!liked) },
-    text
+  return <button onClick={() => setLiked(!liked)}>{text}</button>;
+}
+
+function Container() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <LikeButton />
+      <div>
+        <span>현재 카운트: </span>
+        <span style={{ marginRight: 10 }}>{count}</span>
+        <button onClick={() => setCount(count + 1)}>증가</button>
+        <button onClick={() => setCount(count - 1)}>감소</button>
+      </div>
+    </div>
   );
 }
-const domContainer1 = document.getElementById("root1");
+const domContainer1 = document.getElementById("root");
 ReactDOM.render(React.createElement(LikeButton), domContainer1);
-const domContainer2 = document.getElementById("root2");
-ReactDOM.render(React.createElement(LikeButton), domContainer2);
-const domContainer3 = document.getElementById("root3");
-ReactDOM.render(React.createElement(LikeButton), domContainer3);
